e8d5864fbdeb4e102d0e1e662aa2df50fd21b376
Mason <park.mason.w@gmail.com>
2018-04-22 10:37:57 -0700

    Empty Chart rendered with axes

---
+++ src/components/Chart/Chart.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react'
+import { scaleBand, scaleLinear } from 'd3-scale'
+
+import data from '../../data'
+import Axes from '../Axes/Axes'
+
+export default class Chart extends Component {
+  constructor() {
+    super()
+    this.xScale = scaleBand()
+    this.yScale = scaleLinear()
+  }
+
+  render() {
+    const margins = { top: 50, right: 20, bottom: 100, left: 60 };
+    const svgDimensions = { width: 800, height: 500 };
+
+    const maxValue = Math.max(...data.map(d => d.value))
+
+    // saleBand type
+    const xScale = this.xScale
+      .padding(0.5)
+      // scaleBand domain should be an array of specific values
+      // in our case, we want ot use movie titles
+      .domain(data.map(d => d.title))
+      .range([margins.left, svgDimensions.width - margins.right])
+
+    // scaleLinear type
+    const yScale = this.yScale
+      // scaleLinear domain required at least two values, min and max
+      .domain([0, maxValue])
+      .range([svgDimensions.height - margins.bottom, margins.top])
+
+    return (
+      <svg width={svgDimensions.width} height={svgDimensions.height}>
+        <Axes
+          scales={{ xScale, yScale }}
+          margins={margins}
+          svgDimensions={svgDimensions}
+        />
+      </svg>
+    );
+  }
+}
